11d087d07bb0885193a8ce8815091714b111335e
cakehonolulu <30508313+cakehonolulu@users.noreply.github.com>
2022-08-03 20:01:08 +0200

    Task2: Add all but #19 queries

--- ej2_ud16_sentencias.sql
+++ ej2_ud16_sentencias.sql
@@ -57,7 +57,48 @@ select sum(PRESUPUESTO) from departamentos;
 
 -- 9. Obtener el numero de empleados en cada departamento.
 -- The select has 2 identities in order to display where the employees are.
-select DEPARTAMENTO, count(DEPARTAMENTO) from empleados group by departamento;
+select DEPARTAMENTO, count(DEPARTAMENTO) from empleados group by DEPARTAMENTO;
 
 -- 10. Obtener un listado completo de empleados, incluyendo por cada empleado los datos del empleado y de su departamento.
-select * from empleados inner join departamentos where empleados.departamento = departamentos.codigo;
+select * from empleados inner join departamentos where empleados.DEPARTAMENTO = departamentos.CODIGO;
+
+-- 11. Obtener un listado completo de empleados, incluyendo el nombre y apellidos del empleado junto al nombre y presupuesto de su departamento.
+select empleados.NOMBRE, empleados.APELLIDOS, empleados.DEPARTAMENTO, empleados.PRESUPUESTO from empleados
+join departamentos where empleados.DEPARTAMENTO = departamentos.CODIGO;
+
+-- 12. Obtener los nombres y apellidos de los empleados que trabajen en departamentos cuyo presupuesto sea mayor de 60.000 eur
+select empleados.NOMBRE, empleados.APELLIDOS from empleados join departamentos where departamentos.PRESUPUESTO > 60000 and empleados.DEPARTAMENTO = departamentos.CODIGO;
+
+-- 13. Obtener los datos de los departamentos cuyo presupuesto es superior al presupues-to medio de todos los departamentos.
+select * from departamentos, (select avg(PRESUPUESTO) as PRESUPUESTO_PROMEDIO from departamentos) depts where departamentos.PRESUPUESTO > depts.PRESUPUESTO_PROMEDIO;
+
+-- 14. Obtener los nombre (únicamente los nombres) de los departamentos que tienen más de dos empleados.
+select depts.NOMBRE from (departamentos as depts inner join empleados on depts.CODIGO = empleados.DEPARTAMENTO) group by depts.NOMBRE having count(depts.NOMBRE) > 2;
+
+-- 15. Añadir un nuevo departamento: 'Calidad', con presupuesto de 40000 eur y código11.
+--	   Añadir un empleado vinculado al departamento recién creado: Esther Vázquez, DNI: 89267109
+
+-- 'departamentos' Insert
+insert into departamentos(CODIGO, NOMBRE, PRESUPUESTO)
+values
+	(11, "Calidad", 40000.00);
+
+-- 'empleados' Insert
+insert into empleados(DNI, NOMBRE, APELLIDOS, DEPARTAMENTO)
+values
+	(89267109, "Esther", "Vázquez", 11);
+    
+-- 16. Aplicar un recorte presupuestario del 10% a todos los departamentos.
+-- 10% = x / 100 (1%) * 10
+update departamentos set PRESUPUESTO = PRESUPUESTO - ((PRESUPUESTO / 100) * 10);
+
+-- 17. Reasignar a los empleados del departamento de investigación ( código 77)
+update empleados set DEPARTAMENTO = 14 where DEPARTAMENTO = 17;
+
+-- 18. Despedir a todos los empleados que trabajan para el departamento de informática ( código 14).
+delete from empleados where DEPARTAMENTO = 14;
+
+-- 19. Despedir a todos los empleados que trabajen para departamentos cuyo presupuesto sea superior  a los 60000 eur.
+
+-- 20. Despedir a todos los empleados.
+delete from empleados;
